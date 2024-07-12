c833063ee0da00809f103ef64f94c058c1e03a7a
Viorel Soltan <joomjoom2011@gmail.com>
2024-07-12 10:31:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static("public"));
 app.use(expressLayout);
 app.set("layout", "layouts/main");
 app.set("view engine", "ejs");
-
+app.set("views", __dirname + "/views");
 // Routes
 const homeRoutes = require("./routes/home");
 
